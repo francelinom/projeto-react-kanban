e881d1ee3af21984215c637d8f771275719184a6
francelino <fmarcilio1989@gmail.com>
2025-01-14 14:33:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from "@radix-ui/themes"
+
+function App() {
+
+  return (
+    <Box>
+      <Heading>
+        React Kanban
+      </Heading>
+    </Box>
+  )
+}
+
+export default App
